2c32fdcec6fd213645f8820b14920b6442ca95b9
Nestea <cy602939074@qq.com>
2019-03-12 19:59:34 +0800

    add bin/scirpt

---
+++ stubs/bin/initGenerator.bat
@@ -0,0 +1,3 @@
+cd ..
+vendor\kamicloud\yet-another-generator\gradlew -p vendor\kamicloud\yet-another-generator release
+pause
---
+++ stubs/bin/initGenerator.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd ..
+./vendor/kamicloud/yet-another-generator/gradlew -p vendor/kamicloud/yet-another-generator release
